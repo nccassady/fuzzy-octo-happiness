7bf1c5a269d3a5ab59d92a02e3373d948226c6ba
Nick Cassady <nccassady@gmail.com>
2015-02-18 19:08:56 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Alternah8::Application.config.secret_key_base = 'ef2adcc2cafc23cc0cbf2793a7d46ca472e5569b873c2bbd9d8c7fa2f3a9366bc1a7b24f2a0b622719f905acb767bc0b02bf0bc6c88e187284a2346ead3a4ee6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Alternah8::Application.config.session_store :cookie_store, key: '_alternah8_session'
